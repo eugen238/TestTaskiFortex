5068382cf5d901cff2b95cdc136f24ea7c053771
eugen238 <96869242+eugen238@users.noreply.github.com>
2023-09-30 11:24:28 +0300

    upload

---
+++ TestTask/Services/Classes/UserService.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Classes
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> GetUser()
+        {
+            return await _context.Users.OrderByDescending(u => u.Orders.Count).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _context.Users.Where(u => u.Status == Enums.UserStatus.Active).ToListAsync();
+        }
+    }
+}
